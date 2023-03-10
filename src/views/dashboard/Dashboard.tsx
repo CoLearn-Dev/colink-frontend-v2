de55a00740a23a0712df072973e979bd4c291691
Bryce Wong <wbryce21@gmail.com>
2023-03-10 05:12:37 -0800

    entry modal

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -1,46 +1,19 @@
 import React, { useEffect } from 'react'
 
 import {
-  CAvatar,
   CButton,
-  CButtonGroup,
   CCard,
   CCardBody,
   CCardFooter,
-  CCardHeader,
+  CCardGroup,
   CCol,
-  CProgress,
+  CModal,
+  CModalBody,
+  CModalFooter,
+  CModalHeader,
+  CModalTitle,
   CRow,
-  CTable,
-  CTableBody,
-  CTableDataCell,
-  CTableHead,
-  CTableHeaderCell,
-  CTableRow,
 } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-import {
-  cibCcAmex,
-  cibCcApplePay,
-  cibCcMastercard,
-  cibCcPaypal,
-  cibCcStripe,
-  cibCcVisa,
-  cibGoogle,
-  cibFacebook,
-  cibLinkedin,
-  cifBr,
-  cifEs,
-  cifFr,
-  cifIn,
-  cifPl,
-  cifUs,
-  cibTwitter,
-  cilCloudDownload,
-  cilPeople,
-  cilUser,
-  cilUserFemale,
-} from '@coreui/icons'
 
 import {
   defineFileAction,
@@ -50,7 +23,6 @@ import {
   FileBrowser,
   FileContextMenu,
   FileData,
-  FileHelper,
   FileList,
   FileNavbar,
   FileToolbar,
@@ -58,12 +30,18 @@ import {
 } from 'chonky'
 
 import { ChonkyIconFA } from 'chonky-icon-fontawesome'
-import { useCallback, useMemo, useState } from 'react'
+import { useCallback, useState } from 'react'
 
-import DemoFsMap from './demo.fs_map.json'
 import { useDispatch, useSelector } from 'react-redux'
-import { getUserStorageEntries, keyNameFromPath } from 'src/lib'
+import {
+  getUserStorageEntries,
+  keyNameFromPath,
+  lastKeyNameFromPath,
+  readEntry,
+  storageEntryToJSON,
+} from 'src/lib'
 import { StorageEntry } from 'src/proto_js/colink_pb'
+import { createDownloadHref } from 'src/utils'
 
 setChonkyDefaults({ iconComponent: ChonkyIconFA })
 
@@ -79,11 +57,10 @@ const getNewFiles = async (path: string, getFiles: Function): Promise<FileArray>
   let prefix: string = getPrefixFromPath(path)
   let files: FileArray = []
   await getFiles(prefix).then((keys: string[]) => {
-    console.log(keys)
     files = Array.from(keys).map((key: string) => {
       let entry: FileData = {
         id: key,
-        name: keyNameFromPath(key),
+        name: lastKeyNameFromPath(key),
         isDir: isDirectory(key),
         isHidden: false,
       }
@@ -109,9 +86,9 @@ const getFolderChain = async (path: string): Promise<FileArray> => {
     let fullPath = start + end
     let entry: FileData = {
       id: fullPath,
-      name: keyNameFromPath(fullPath),
-      isHidden: false,
+      name: lastKeyNameFromPath(fullPath),
       isDir: isDirectory(fullPath),
+      isHidden: false,
     }
     folderChain.unshift(entry)
 
@@ -128,12 +105,17 @@ const getFolderChain = async (path: string): Promise<FileArray> => {
   return Promise.resolve(folderChain)
 }
 
-export const useFileActionHandler = (setPath: Function) => {
+export const useFileActionHandler = (setPath: Function, displayEntry: Function) => {
   return useCallback(
     (data: ChonkyFileActionData) => {
       if (data.id === ChonkyActions.OpenFiles.id) {
         if (typeof data.payload.targetFile !== 'undefined') {
-          setPath(data.payload.targetFile.id)
+          if (data.payload.targetFile.isDir) {
+            // go to next path/series of entries
+            setPath(data.payload.targetFile.id)
+          } else {
+            displayEntry(keyNameFromPath(data.payload.targetFile.id))
+          }
         }
       }
     },
@@ -158,6 +140,7 @@ const ReadOnlyVFSBrowser: React.FC<{
   path: string
   setPath: Function
   getKeys: Function
+  displayEntry: Function
 }> = (props) => {
   const [files, setFiles] = useState<FileArray<FileData>>([])
   const [folderChain, setFolderChain] = useState<FileArray<FileData>>([])
@@ -179,7 +162,7 @@ const ReadOnlyVFSBrowser: React.FC<{
     updateFolderChain()
   }, [props.path])
 
-  const handleFileAction = useFileActionHandler(props.setPath)
+  const handleFileAction = useFileActionHandler(props.setPath, props.displayEntry)
   return (
     <div style={{ height: 400 }}>
       <FileBrowser
@@ -188,9 +171,7 @@ const ReadOnlyVFSBrowser: React.FC<{
         fileActions={myFileActions}
         folderChain={folderChain}
         onFileAction={handleFileAction}
-        thumbnailGenerator={(file: FileData) =>
-          file.thumbnailUrl ? `https://chonky.io${file.thumbnailUrl}` : null
-        }
+        thumbnailGenerator={(file: FileData) => null}
       >
         <FileNavbar />
         <FileToolbar />
@@ -206,11 +187,18 @@ const Dashboard: React.FC = () => {
   const { client } = useSelector((state: any) => state.client)
   const dispatch = useDispatch()
 
+  // Keep track of current key path for file navigation
   const [path, setPath] = useState('')
+
+  // Variables for displaying entry information
+  const defEmpty = new StorageEntry()
+  const [entry, updateDisplay] = useState(defEmpty)
+  const [visible, updateVisible] = useState(false)
+  const [isString, updateB64] = useState(true)
 
   async function getKeysAtPrefix(prefix: string) {
     let paths: string[] = []
-    if (prefix == '') {
+    if (prefix === '') {
       await getUserStorageEntries(client, jwt).then((entries: StorageEntry[]) => {
         paths = entries.map((entry: StorageEntry) => {
           return entry.getKeyPath()
@@ -226,8 +214,15 @@ const Dashboard: React.FC = () => {
     return Promise.resolve(paths)
   }
 
+  async function displayEntry(keyName: string) {
+    await readEntry(client, jwt, keyName).then((entry: StorageEntry) => {
+      updateDisplay(entry)
+      updateVisible(true)
+    })
+  }
+
   useEffect(() => {
-    if (jwt != '' && client != '') {
+    if (jwt !== '' && client !== '') {
       setPath('@')
     }
   }, [jwt, client])
@@ -251,10 +246,64 @@ const Dashboard: React.FC = () => {
               path={path}
               setPath={setPath}
               getKeys={getKeysAtPrefix}
+              displayEntry={displayEntry}
             />
           </CRow>
         </CCardFooter>
       </CCard>
+
+      <CModal
+        size="lg"
+        backdrop="static"
+        style={{ maxWidth: '75%', marginLeft: '12.5%' }}
+        visible={visible}
+        onClose={() => updateVisible(false)}
+        alignment="center"
+      >
+        <CModalHeader>
+          <CModalTitle>Entry: {visible ? lastKeyNameFromPath(entry.getKeyPath()) : ''}</CModalTitle>
+        </CModalHeader>
+        <CModalBody>
+          <CCardGroup>
+            <CCard></CCard>
+          </CCardGroup>
+          <CCardGroup>
+            <CCard className="p-2">
+              <CCardBody className="text-center">
+                <div
+                  style={{
+                    whiteSpace: 'pre-wrap',
+                    textAlign: 'left',
+                    overflowWrap: 'break-word',
+                    width: '100%',
+                  }}
+                >
+                  {visible ? JSON.stringify(storageEntryToJSON(entry, isString), null, 2) : ''}
+                </div>
+                <CButton className="ms-2" active tabIndex={-1} color="info">
+                  <a
+                    download="payload.txt"
+                    href={
+                      visible
+                        ? createDownloadHref(
+                            JSON.stringify(storageEntryToJSON(entry, isString), null, 2),
+                          )
+                        : '#'
+                    }
+                  >
+                    Download Entry
+                  </a>
+                </CButton>
+              </CCardBody>
+            </CCard>
+          </CCardGroup>
+        </CModalBody>
+        <CModalFooter>
+          <CButton color="secondary" onClick={() => updateVisible(false)}>
+            Close
+          </CButton>
+        </CModalFooter>
+      </CModal>
     </>
   )
 }
